57326cd6a6ad27f283f24334d459e13b5623b1a3
Bhavesh Zalke <zalkebhavesh@gmail.com>
2024-12-11 21:53:05 +0530

    Day 4

---
+++ day4.js
@@ -0,0 +1,18 @@
+const arr = [1, 2, 3, 4, 5]
+
+// delete (arr[1])
+
+// delete function delete the item but not the refernce
+console.log(arr)
+
+const obj1 = { a: 1 }
+const obj2 = { a: 1 }
+
+// the object compare by referce not by value
+
+// console.log(obj1 === obj2)
+
+const arr2 = [1, 2, 3, 4, 5]
+console.log(arr2 == arr)
+
+// same as array 
