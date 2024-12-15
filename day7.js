3f222f67ebee836fe84517edec248dec4ff023ce
Bhavesh Zalke <zalkebhavesh@gmail.com>
2024-12-15 10:46:24 +0530

    Day 7

---
+++ day7.js
@@ -0,0 +1,24 @@
+function show() {
+    {
+        let x = 10
+        const y = 20
+        console.log(x)
+    }
+    // console.log(y)
+    console.log("bhavesh")
+}
+
+
+// show()
+
+
+// const arr = []
+// console.log(arr.length)
+// arr[10] = 20
+// console.log(arr.length)
+
+// arr[100] = 123
+// console.log(arr.length)
+// console.log(arr)
+
+// arr.forEach(e => console.log("jfh"))
