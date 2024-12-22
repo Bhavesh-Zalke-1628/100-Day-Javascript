3e87ebc68482f804f65daa0f5bd3c78d9703fdc6
Bhavesh Zalke <zalkebhavesh@gmail.com>
2024-12-22 21:32:05 +0530

    Day 13

---
+++ day13.js
@@ -0,0 +1,83 @@
+// function abc() {
+//     console.log(abc.xyz)
+// }
+
+
+// abc()
+// abc.xyz = 400
+// abc.xyz = 200
+// abc()
+
+
+// event propogation
+
+// const arr = [1, 2, 3, 4]
+// arr[100] = 500
+// console.log(arr)
+
+
+// console.log(typeof typeof 100)   // type number
+
+
+// const arr = [..."bbha"]
+// console.log(arr)
+
+// console.log(parseInt("10+2"))
+// console.log(parseInt('7FM'))
+// console.log(parseInt("M7F"))/
+
+// console.log([1, 2].map((e) => {
+//     if (e > 0) return
+//     return e * 2
+// }))
+
+
+// "use strict"
+// {
+//     function abc() {
+//         console.log("xyz")
+//     }
+
+//     console.log(abc())
+// }
+
+// abc()
+
+
+
+// function abc(a, b) {
+
+//     a = 100
+//     b = 200
+//     return (arguments[0] + arguments[1])
+//     console.log(a + b)
+// }
+
+// console.log(abc(20, 5))
+
+
+// const arr = [0, 1, 2, 3, 4, 5, 6, 7]
+// console.log(arr.slice(1, 4))
+// console.log(arr)
+// console.log(arr.splice(0, 2))
+// console.log(arr)
+
+const arr = [1, 2, 3, 4, 5, 6, 7, 8]
+
+// const x = arr.filter(e => e > 4)
+// console.log(x)
+// console.log(arr)
+
+
+// const x = arr.map((num) => {
+//     return num > 4
+// })
+// console.log(x)
+
+
+
+
+// webpack
+// void element
+// empty tag
+// psudo classes in css 
