e47c87ffdf7982b4bfdfe9e79a47ed164bf66f89
Bhavesh Zalke <zalkebhavesh@gmail.com>
2024-12-25 17:50:13 +0530

    Day 15

---
+++ day15.js
@@ -0,0 +1,33 @@
+
+//  using recursion
+// const arr = [x => x + 1, x => x * x, x => 2 * x];
+// let x = 5;
+
+// function cal(arr, x, index = arr.length - 1) {
+//     if (index < 0) {
+//         return x;
+//     }
+//     return cal(arr, arr[index](x), index - 1);
+// }
+
+// const result = cal(arr, x);
+// console.log(result);
+
+
+
+//  using compose function
+
+// const arr = [x => x + 1, x => x * x, x => 2 * x];
+// let x = 5;
+
+
+// function compose(functions) {
+//     return function (x) {
+//         return functions.reduceRight((value, func) => func(value), x);
+//     };
+// }
+
+
+
+// let result = compose(arr)
+// console.log(result(10))
