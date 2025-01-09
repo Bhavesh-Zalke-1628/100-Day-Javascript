30cdb3faf7e7230b6c1ef6bff44544db105d0c97
Bhavesh Zalke <zalkebhavesh@gmail.com>
2025-01-09 20:51:33 +0530

    Day 30

---
+++ day30.js
@@ -0,0 +1,26 @@
+// process.stdin.setEncoding('utf8');
+// process.stdin.on('data', function (input) {
+//     const num = input.split('');
+//     console.log(num[0])
+//     const testcase = num[0];
+//     for (let index = 1; index < testcase; index++) {
+//         console.log(index * 2)
+
+//     }
+// });
+
+
+
+
+// Debug/fix the code
+
+
+process.stdin.setEncoding('utf8');
+process.stdin.on('data', function (input) {
+    const nums = input.split('\n');
+    const testCases = nums[0];
+    for (i = 1; i <= testCases; i++) {
+
+    }
+
+});
