a24f87dfc311e5f01332cff6902a3279fc4738c8
Bhavesh Zalke <zalkebhavesh@gmail.com>
2025-01-13 21:43:10 +0530

    Day 33

---
+++ day33.js
@@ -0,0 +1,14 @@
+const obj1 = {
+    name: "bhavesh"
+}
+
+
+const obj2 = {
+    age: 21
+}
+
+console.log(obj1)
+// const employee = Object.assign({}, obj1, obj2) // create the new object emmplyee assign to value new object
+// const employee = Object.assign(obj1, obj2)  //update the obj1 directly 
+console.log(employee)
+console.log(obj1)
