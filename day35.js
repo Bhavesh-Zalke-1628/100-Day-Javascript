e0242e5a7c5a9daf0c363638d26bc9359ff25447
Bhavesh Zalke <zalkebhavesh@gmail.com>
2025-01-14 23:26:34 +0530

    Day 35

---
+++ day35.js
@@ -0,0 +1,34 @@
+// function 
+
+
+
+// function function_name  (){   // function declaration
+//     // statement 
+// }
+
+
+// function_name(); //fucntion call
+
+
+
+
+function show() {
+    console.log(
+        "Hello"
+    )
+}
+
+
+// show();
+
+
+
+function abc(x, y) {
+    return x + y;
+}
+
+
+console.log(
+
+    abc(10, 20)
+)
