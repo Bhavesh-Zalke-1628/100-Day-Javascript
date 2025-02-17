227506de21956c16cddf9b1923010e21ced78a7a
Bhavesh Zalke <zalkebhavesh@gmail.com>
2025-02-17 22:27:11 +0530

    Day 43

---
+++ day43.js
@@ -0,0 +1,12 @@
+class basic {
+    constructor(name, email) {
+        this.name = name,
+            this.email = email
+    }
+}
+
+
+
+const obj1 = new basic("bhavesh", "b@gmail.com");
+
+console.log(obj1)
