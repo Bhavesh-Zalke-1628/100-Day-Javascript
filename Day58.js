e52d7d54405b39865457f855023a71c02524a74a
Bhavesh Zalke <zalkebhavesh@gmail.com>
2025-07-31 22:54:24 +0530

    day 58

---
+++ Day58.js
@@ -0,0 +1,36 @@
+// // const arr = [
+// //     { name: "John", age: 21 },
+// //     { name: "Mike", age: 28 },
+// // ]
+
+
+
+// // const ageArr = []
+// // arr.map(ele => {
+// //     ageArr.push(ele.age)
+// // })
+
+// // console.log(ageArr)
+
+
+// const arr = [1, 2, 3, 4, 5, 6, 66, 4, 33, 44, 66, 77, 8, 33, 22]
+// // const even = []
+// // const odd = []
+// // arr.map(e => e % 2 == 0 ? even.push(e) : odd.push(e))
+// // console.log(even)
+// // console.log(odd)
+
+// const data = arr.reduce(
+//     (acc, e) => {
+//         e % 2 === 0 ? acc.even.push(e) : acc.odd.push(e);
+//         return acc;
+//     },
+//     { even: [], odd: [] }
+// );
+
+// // console.log(even);
+// // console.log(odd);
+
+
+
+// console.log(data)
