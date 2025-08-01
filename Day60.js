0fc9adf225b1b0fcb6e71fe73633199518fdf5d8
Bhavesh Zalke <zalkebhavesh@gmail.com>
2025-08-01 23:38:38 +0530

    day 60

---
+++ Day60.js
@@ -0,0 +1,17 @@
+
+// const res = ++[[]][0]
+// const x = [+[]]
+// console.log(res + x)
+
+
+console.log([] + []);
+console.log([] + {});
+console.log({} + []);
+console.log({} + {});
+
+
+console.log(typeof NaN);
+console.log(NaN === NaN);
+console.log(Number(NaN));
+console.log(isNaN(false));
+
